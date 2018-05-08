b305fcfd5a040659d67d04b5c68c4180f7672751
Amaranta Lukas <34173491+takalu18@users.noreply.github.com>
2018-05-08 23:19:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
 var phoneBtn = document.getElementById("phone-btn");
 var phoneNumbers = document.getElementById("phone-numbers");
 
+
+var menuBtnOpen = document.getElementById("btn-open");
+var menuBtnClose = document.getElementById("btn-close");
+var headerInfo = document.getElementById("header-info");
+var mainNav = document.getElementById("main-nav");
+
+
 phoneBtn.addEventListener("click", function(event) {
   event.preventDefault();
   phoneNumbers.classList.toggle("modal-content-show");
+});
+
+
+menuBtnOpen.addEventListener("click", function(event) {
+  event.preventDefault();
+  menuBtnClose.classList.add("modal-content-show");
+  headerInfo.classList.add("modal-content-show");
+  mainNav.classList.add("modal-content-show");
+});
+
+
+menuBtnClose.addEventListener("click", function(event) {
+  event.preventDefault();  
+  menuBtnClose.classList.remove("modal-content-show");  
+  headerInfo.classList.remove("modal-content-show");
+  mainNav.classList.remove("modal-content-show");
+});
+
+
+
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (menuBtnClose.classList.contains("modal-content-show")) {
+	  menuBtnClose.classList.remove("modal-content-show");  
+      headerInfo.classList.remove("modal-content-show");
+      mainNav.classList.remove("modal-content-show");
+	}
+  }
 });
